0420aeeda49c7e9469fcb821f7d81500b228b1e0
mtalha218 <muhammadtalha218218@gmail.com>
2023-09-26 22:46:37 +0500

    📦 NEW: Services animation / Footer

--- src/Pages/Home/Footer.jsx
+++ src/Pages/Home/Footer.jsx
@@ -1,40 +1,100 @@
-import React from 'react';
-import logo from "../../assets/intelektx_logo.png"
+import React from "react";
+import logo from "../../assets/intelektx_logo.png";
 
 const Footer = () => {
   return (
-    <footer className="footer-shadow text-white py-10 px-[20%] mx-auto">
-      <div className="container flex justify-between">
-        
-        
-        <div className='flex gap-32'>
-        <nav>
-            <p className='mb-5'>Links</p>
-          <ul className="">
-            <li className='mb-2'><a className='text-xs opacity-60 ' href="/">Home</a></li>
-            <li className='mb-2'><a className='text-xs opacity-60' href="/services">Services</a></li>
-            <li className='mb-2'><a className='text-xs opacity-60' href="/about">About</a></li>
-            <li className='mb-2'><a className='text-xs opacity-60' href="/contact">Contact</a></li>
-          </ul>
-        </nav>
+    <footer className="footer-shadow text-white py-10 px-[10%] mx-auto max-md:px-0 max-md:w-[90%]">
+      <div className="container flex gap-52 max-md:gap-16 max-md:gap-16 max-sm:gap-4 justify-center w-[100%]">
+          <nav>
+            <p className="mb-5">Links</p>
+            <ul className="">
+              <li className="mb-2">
+                <a className="text-xs opacity-60 " href="/">
+                  Home
+                </a>
+              </li>
+              <li className="mb-2">
+                <a className="text-xs opacity-60" href="/services">
+                  Services
+                </a>
+              </li>
+              <li className="mb-2">
+                <a className="text-xs opacity-60" href="/about">
+                  About
+                </a>
+              </li>
+              <li className="mb-2">
+                <a className="text-xs opacity-60" href="/contact">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </nav>
 
-        <nav>
-            <p className='mb-5'>Community</p>
-          <ul className="">
-            <li className='mb-2'><a className='text-xs opacity-60 ' href="/">Upwork</a></li>
-            <li className='mb-2'><a className='text-xs opacity-60' href="/services">LinkedIn</a></li>
-            <li className='mb-2'><a className='text-xs opacity-60' href="/about">Instagram</a></li>
-            <li className='mb-2'><a className='text-xs opacity-60' href="/contact">Twitter</a></li>
-          </ul>
-        </nav>
-        </div>
+          <nav>
+            <p className="mb-5">Solutions</p>
+            <ul className="">
+              <li className="mb-2">
+                <a className="text-xs opacity-60 " href="/">
+                  Computer Vision
+                </a>
+              </li>
+              <li className="mb-2">
+                <a className="text-xs opacity-60" href="/services">
+                  Machine Learning
+                </a>
+              </li>
+              <li className="mb-2">
+                <a className="text-xs opacity-60" href="/about">
+                  Deep Learning
+                </a>
+              </li>
+              <li className="mb-2">
+                <a className="text-xs opacity-60" href="/contact">
+                  Data Analytics
+                </a>
+              </li>
+              <li className="mb-2">
+                <a className="text-xs opacity-60" href="/contact">
+                  Full Stack Development
+                </a>
+              </li>
+              <li className="mb-2">
+                <a className="text-xs opacity-60" href="/contact">
+                  AR / VR
+                </a>
+              </li>
+            </ul>
+          </nav>
 
-        <div className="flex items-center">
-          <img src={logo} alt="Company Logo" className="w-[150px]" />
-        </div>
+          <nav>
+            <p className="mb-5">Community</p>
+            <ul className="">
+              <li className="mb-2">
+                <a className="text-xs opacity-60 " href="/">
+                  Upwork
+                </a>
+              </li>
+              <li className="mb-2">
+                <a className="text-xs opacity-60" href="/services">
+                  LinkedIn
+                </a>
+              </li>
+              <li className="mb-2">
+                <a className="text-xs opacity-60" href="/about">
+                  Instagram
+                </a>
+              </li>
+              <li className="mb-2">
+                <a className="text-xs opacity-60" href="/contact">
+                  Twitter
+                </a>
+              </li>
+            </ul>
+          </nav>
       </div>
-      
-      <div className="text-center mt-4 text-xs">
+
+      <div className="text-center mt-12 text-xs">
         <p>&copy; {new Date().getFullYear()} Intelektx. All Rights Reserved.</p>
       </div>
     </footer>
--- src/Pages/Home/Services.jsx
+++ src/Pages/Home/Services.jsx
@@ -14,57 +14,115 @@ const Services = () => {
         <div className="w-[100%]">
           <p className="text-heading1 w-fit mx-auto">Our Services</p>
           <div className="flex flex-wrap gap-10 justify-between mt-20">
-          <div
-          className="bg-[#1b0d38] flex justify-center items-center px-10 py-10 max-lg:w-[100%] w-[30%] min-h-[300px] mx-auto"
-          style={{ boxShadow: " 0px 25px 20px -20px #361E5B45" }}
-        >
-          <div>
-          <img src={vision} className="mx-auto w-[60px]" alt="services"/>
-          <p className="text-center mt-5">Computer Vision</p>
-          </div>
-        </div>
+            {/* __________________________________________________________________________________________ */}
 
-        <div
-          className="bg-[#1b0d38] flex justify-center items-center px-10 py-10 max-lg:w-[100%] w-[30%] min-h-[300px] mx-auto"
-          style={{ boxShadow: " 0px 25px 20px -20px #361E5B45" }}
-        >
-          <div>
-          <img src={deep} className="mx-auto w-[60px]" alt="services"/>
-          <p className="text-center mt-5">Deep Learning</p>
-          </div>
-        </div>
+            <div className="flip-card  max-lg:w-[100%] w-[30%] min-h-[300px] mx-auto">
+              <div className="flip-card-inner bg-[#1b0d38] w-[100%] ">
+                <div className="flip-card-front flex flex-col justify-center items-center">
+                  <img
+                    src={vision}
+                    className="mx-auto w-[60px]"
+                    alt="services"
+                  />
+                  <p className="text-center mt-5">Computer Vision</p>
+                </div>
+                <div className="flip-card-back flex justify-center items-center text-center p-4">
+                  <p style={{color:"#090315"}}>Image Recognition, Object Detection, Facial Analysis, Gesture Recognition, Semantic Segmentation, Feature Extraction, Pose Estimation, Scene Understanding, Video Tracking, Depth Estimation, Image Registration, Medical Imaging, Emotion Analysis, Activity Recognition</p>
+                </div>
+              </div>
+            </div>
 
-        <div
-          className="bg-[#1b0d38] flex justify-center items-center px-10 py-10 max-lg:w-[100%] w-[30%] min-h-[300px] mx-auto"
-          style={{ boxShadow: " 0px 25px 20px -20px #361E5B45" }}
-        >
-          <div>
-          <img src={machine} className="mx-auto w-[60px]" alt="services"/>
-          <p className="text-center mt-5">Machine Learning</p>
-          </div>
-        </div>
+            {/* __________________________________________________________________________________________ */}
 
-        <div
-          className="bg-[#1b0d38] flex justify-center items-center px-10 py-10 max-lg:w-[100%] w-[30%] min-h-[300px] mx-auto"
-          style={{ boxShadow: " 0px 25px 20px -20px #361E5B45" }}
-        >
-          <div>
-          <img src={data} className="mx-auto w-[60px]" alt="services"/>
-          <p className="text-center mt-5">Data Analytics</p>
-          </div>
-        </div>
+            <div className="flip-card  max-lg:w-[100%] w-[30%] min-h-[300px] mx-auto">
+              <div className="flip-card-inner bg-[#1b0d38] w-[100%] ">
+                <div className="flip-card-front flex flex-col justify-center items-center">
+                  <img
+                    src={vision}
+                    className="mx-auto w-[60px]"
+                    alt="services"
+                  />
+                  <p className="text-center mt-5">Deep Learning</p>
+                </div>
+                <div className="flip-card-back flex justify-center items-center text-center p-4">
+                  <p style={{color:"#090315"}}>Neural Networks, Convolutional Neural Networks (CNNs), Recurrent Neural Networks (RNNs), Natural Language Processing (NLP), Speech Recognition, Deep Reinforcement Learning, Deep Learning in Healthcare, Deep Learning for Recommender Systems.</p>
+                </div>
+              </div>
+            </div>
 
-        <div
-          className="bg-[#1b0d38] flex justify-center items-center px-10 py-10 max-lg:w-[100%] w-[30%] min-h-[300px] mx-auto"
-          style={{ boxShadow: " 0px 25px 20px -20px #361E5B45" }}
-        >
-          <div>
-          <img src={web} className="mx-auto w-[60px]" alt="services"/>
-          <p className="text-center mt-5">Full Stack Development</p>
-          </div>
-        </div>
+            {/* __________________________________________________________________________________________ */}
 
-        <div
+            <div className="flip-card  max-lg:w-[100%] w-[30%] min-h-[300px] mx-auto">
+              <div className="flip-card-inner bg-[#1b0d38] w-[100%] ">
+                <div className="flip-card-front flex flex-col justify-center items-center">
+                  <img
+                    src={vision}
+                    className="mx-auto w-[60px]"
+                    alt="services"
+                  />
+                  <p className="text-center mt-5">Machine Learning</p>
+                </div>
+                <div className="flip-card-back flex justify-center items-center text-center p-4">
+                  <p style={{color:"#090315"}}>Computer Vision, Generative AI, 2D/3D Object Detection, Segmentation, Data Labeling, Data Augmentation, Pose Estimation, Reinforcement Learning, Edge AI, Hardware Acceleration, Deployment, Machine Learning, Statistical Modeling, Classification, Time Series Analysis, Event Monitoring</p>
+                </div>
+              </div>
+            </div>
+
+            {/* __________________________________________________________________________________________ */}
+
+            <div className="flip-card  max-lg:w-[100%] w-[30%] min-h-[300px] mx-auto">
+              <div className="flip-card-inner bg-[#1b0d38] w-[100%] ">
+                <div className="flip-card-front flex flex-col justify-center items-center">
+                  <img
+                    src={vision}
+                    className="mx-auto w-[60px]"
+                    alt="services"
+                  />
+                  <p className="text-center mt-5">Data Analytics</p>
+                </div>
+                <div className="flip-card-back flex justify-center items-center text-center p-4">
+                  <p style={{color:"#090315"}}>Data Visualization, Exploratory Data Analysis, Data Cleaning, Statistical Analysis, Machine Learning for Data Analysis, Big Data Analytics, Data Mining, A/B Testing, Data-driven Decision Making, Business Intelligence, Data Storytelling, Text Analytics, Customer Segmentation, Anomaly Detection.</p>
+                </div>
+              </div>
+            </div>
+
+            {/* __________________________________________________________________________________________ */}
+
+            <div className="flip-card  max-lg:w-[100%] w-[30%] min-h-[300px] mx-auto">
+              <div className="flip-card-inner bg-[#1b0d38] w-[100%] ">
+                <div className="flip-card-front flex flex-col justify-center items-center">
+                  <img
+                    src={vision}
+                    className="mx-auto w-[60px]"
+                    alt="services"
+                  />
+                  <p className="text-center mt-5">Full Stack Development</p>
+                </div>
+                <div className="flip-card-back flex justify-center items-center text-center p-4">
+                  <p style={{color:"#090315"}}>Front-end Development, Back-end Development, Web Development Frameworks, Responsive Web Design, Databases and SQL, RESTful APIs, Cloud Computing, DevOps and CI/CD, Containerization (Docker), Web Security, User Experience (UX) Design, Cross-platform Development</p>
+                </div>
+              </div>
+            </div>
+
+            {/* __________________________________________________________________________________________ */}
+
+            <div className="flip-card  max-lg:w-[100%] w-[30%] min-h-[300px] mx-auto">
+              <div className="flip-card-inner bg-[#1b0d38] w-[100%] ">
+                <div className="flip-card-front flex flex-col justify-center items-center">
+                  <img
+                    src={vision}
+                    className="mx-auto w-[60px]"
+                    alt="services"
+                  />
+                  <p className="text-center mt-5">AR / VR</p>
+                </div>
+                <div className="flip-card-back  flex justify-center items-center text-center p-4">
+                  <p style={{color:"#090315"}}>AR/VR Applications, Immersive Experiences, AR/VR Content Creation, 3D Modeling for AR/VR, AR/VR in Gaming, AR/VR in Education, AR/VR in Healthcare, AR/VR Interaction Design, AR/VR User Interfaces, AR/VR Simulations, AR/VR Hardware Advances.</p>
+                </div>
+              </div>
+            </div>
+
+            {/* <div
           className="bg-[#1b0d38] flex justify-center items-center px-10 py-10 max-lg:w-[100%] w-[30%] min-h-[300px] mx-auto"
           style={{ boxShadow: " 0px 25px 20px -20px #361E5B45" }}
         >
@@ -72,11 +130,10 @@ const Services = () => {
           <img src={ar} className="mx-auto w-[60px]" alt="services"/>
           <p className="text-center mt-5">AR / VR</p>
           </div>
-        </div>
+        </div> */}
           </div>
           <ButtonPrimary text={"Explore More"} className="mt-20 mx-auto" />
         </div>
-       
       </div>
     </div>
   );
